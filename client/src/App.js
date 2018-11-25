635f1794e4e263346a7a07b185c9336e734e6b90
nathanbaleeta <nbaleeta@gmail.com>
2018-11-25 22:43:54 +0300

    First commit

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { CssBaseline, withStyles } from "@material-ui/core";
+
+import Header from './Components/Header'
+import Dashboard from "./Pages/Dashboard";
+
+const styles = theme => ({
+  main: {
+    padding: 3 * theme.spacing.unit,
+    [theme.breakpoints.down("xs")]: {
+      padding: 2 * theme.spacing.unit
+    }
+  }
+});
+
+const App = ({ classes }) => (
+  <React.Fragment>
+    <CssBaseline />
+    <Header />
+    <main className={classes.main}>
+      <Dashboard />
+    </main>
+  </React.Fragment>
+);
+
+export default withStyles(styles)(App);
